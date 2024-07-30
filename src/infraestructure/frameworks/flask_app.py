ae6c2c3e816bd2506bffd59dc8ff09f3030e3612
Daniel Braz <danielbrazlopes16@gmail.com>
2024-07-30 19:02:03 -0300

    feat(main): configure injector

--- src/infraestructure/frameworks/flask_app.py
+++ src/infraestructure/frameworks/flask_app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_injector import FlaskInjector
+from src.infraestructure.di.container import DI
 
 class App:
   @staticmethod
   def create_app():
     app = Flask(__name__)
+    App.configure_injector(app)
     return app
+
+  @staticmethod
+  def configure_injector(app):
+    FlaskInjector(app=app, modules=[DI.configure])
