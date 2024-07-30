a83b9d9bccea646494b6635ff531d680abb1a1a7
Daniel Braz <danielbrazlopes16@gmail.com>
2024-07-30 18:15:54 -0300

    refactor(main): implement config interface

--- src/infraestructure/config/config.py
+++ src/infraestructure/config/config.py
@@ -1,6 +1,7 @@
 import os
+from src.core.interfaces.config_interface import ConfigInterface
 
-class Config:
+class Config(ConfigInterface):
   PORT = os.getenv('PORT')
   ENVIRONMENT = os.getenv('ENVIRONMENT')
   VERSION = os.getenv('API_VERSION')
