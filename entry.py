e4daa9490e45c2ab72ff7b2c775e851f978ad903
Daniel Braz <danielbrazlopes16@gmail.com>
2024-07-30 18:01:30 -0300

    refactor(main): move main layer into infraestructure layer

--- entry.py
+++ entry.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from src.main.server.server import Server
-from src.main.config.config import Config
+from src.infraestructure.server.server import Server
+from src.infraestructure.config.config import Config
 
 if __name__ == '__main__':
   Server().listen(port=Config.PORT, env=Config.ENVIRONMENT)
