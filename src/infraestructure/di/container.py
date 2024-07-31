bd0bf2f6c1dda7eee81e43f48f83968b5d82491f
Daniel Braz <danielbrazlopes16@gmail.com>
2024-07-31 14:40:56 -0300

    feat(main): inject index one user use case

--- src/infraestructure/di/container.py
+++ src/infraestructure/di/container.py
@@ -1,11 +1,14 @@
 from injector import Binder, singleton
-from src.infraestructure.interfaces.network.http_status_interface import HttpStatusInterface
-from src.application.interfaces.repositories.user_repository_interface import UserRepositoryInterface
 from src.infraestructure.network.http_status import HttpStatus
+from src.infraestructure.interfaces.network.http_status_interface import HttpStatusInterface
 from src.infraestructure.repositories.sql_alchemy_user_repository import SqlAlchemyUserRepository
+from src.application.interfaces.repositories.user_repository_interface import UserRepositoryInterface
+from src.application.interfaces.use_cases.index_one_user_use_case_interface import IndexOneUserUseCaseInterface
+from src.application.use_cases.index_one_user_use_case import IndexOneUserUseCase
 
 class DI:
   @staticmethod
   def configure(binder: Binder):
     binder.bind(HttpStatusInterface, to=HttpStatus, scope=singleton)
     binder.bind(UserRepositoryInterface, to=SqlAlchemyUserRepository, scope=singleton)
+    binder.bind(IndexOneUserUseCaseInterface, to=IndexOneUserUseCase, scope=singleton)
