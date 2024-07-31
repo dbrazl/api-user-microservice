129e9be70b5dca7b2618c2b9766297028da0d4f9
Daniel Braz <danielbrazlopes16@gmail.com>
2024-07-31 11:03:40 -0300

    feat(main): init database

---
+++ src/infraestructure/datasources/__init__.py
@@ -0,0 +1,14 @@
+from src.infraestructure.datasources.sql_alchemy_database_connector import SqlAlchemyDatabaseConnector
+from src.infraestructure.datasources.sql_alchemy_database_schema import SqlAlchemyDatabaseSchema
+from src.infraestructure.datasources.sql_alchemy_environment_manager import SqlAlchemyEnvironmentManager
+
+database_connector = SqlAlchemyDatabaseConnector()
+engine = database_connector.connect()
+
+database_schema = SqlAlchemyDatabaseSchema()
+Base = database_schema.create_base()
+
+environment_manager = SqlAlchemyEnvironmentManager(Base, engine)
+environment_manager.listen_changes()
+
+__all__ = ["engine", "Base"]
