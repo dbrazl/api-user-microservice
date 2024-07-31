330ed6329354a5fe3a23a537c049f67dae6b4e0f
Daniel Braz <danielbrazlopes16@gmail.com>
2024-07-31 11:28:59 -0300

    feat(main): create sql alchemy user repository

---
+++ src/infraestructure/repositories/sql_alchemy_user_repository.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import sessionmaker
+from src.infraestructure.datasources import engine
+from src.infraestructure.schemas.sql_alchemy_user_schema import SqlAlchemyUserSchema
+from src.core.interfaces.repositories.user_repository_interface import UserRepositoryInterface
+import uuid
+
+class SqlAlchemyUserRepository(UserRepositoryInterface):
+  def __init__(self):
+    Session = sessionmaker(bind=engine)
+    self.session = Session()
+
+  def index_one(self, user_id):
+    return self.session.query(SqlAlchemyUserSchema).filter_by(id=uuid.UUID(user_id)).first()
