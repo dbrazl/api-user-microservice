1d6446eeb52972a59dd6615bf41d979893ee339a
Daniel Braz <danielbrazlopes16@gmail.com>
2024-07-31 09:01:21 -0300

    feat(main): create index one endpoint skeleton

--- src/adapters/controllers/user_controller.py
+++ src/adapters/controllers/user_controller.py
@@ -8,3 +8,8 @@ class UserController:
   @user_bp.route('/health', methods=['GET'])
   def health(http_status: HttpStatusInterface):
     return make_response('', http_status.OK)
+
+  @staticmethod
+  @user_bp.route('/<string:user_id>', methods=['GET'])
+  def index_one(user_id, http_status: HttpStatusInterface):
+    return make_response('', http_status.OK)
