8b5a2e192888cbc982d708965ca1d7d4885445a4
Daniel Braz <danielbrazlopes16@gmail.com>
2024-07-31 11:34:43 -0300

    feat(main): inject user repository

--- src/infraestructure/di/container.py
+++ src/infraestructure/di/container.py
@@ -1,8 +1,11 @@
 from injector import Binder, singleton
 from src.core.interfaces.network.http_status_interface import HttpStatusInterface
+from src.core.interfaces.repositories.user_repository_interface import UserRepositoryInterface
 from src.infraestructure.network.http_status import HttpStatus
+from src.infraestructure.repositories.sql_alchemy_user_repository import SqlAlchemyUserRepository
 
 class DI:
   @staticmethod
   def configure(binder: Binder):
     binder.bind(HttpStatusInterface, to=HttpStatus, scope=singleton)
+    binder.bind(UserRepositoryInterface, to=SqlAlchemyUserRepository, scope=singleton)
