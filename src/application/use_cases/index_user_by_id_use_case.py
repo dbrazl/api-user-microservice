c063c7352f5711d52db34a539dd9e7e889687c21
Daniel Braz <danielbrazlopes16@gmail.com>
2024-07-31 21:29:55 -0300

    feat(main): implement use case to index user by id

---
+++ src/application/use_cases/index_user_by_id_use_case.py
@@ -0,0 +1,13 @@
+from injector import inject
+from src.application.interfaces.use_cases.index_user_by_id_use_case_interface import IndexUserByIdUseCaseInterface
+from src.application.interfaces.repositories.user_repository_interface import UserRepositoryInterface
+from src.application.dtos.user_dto import UserDto
+
+class IndexUserByIdUseCase(IndexUserByIdUseCaseInterface):
+  @inject
+  def __init__(self, user_repository: UserRepositoryInterface) -> None:
+    self.user_repository = user_repository
+
+  def execute(self, user_id: str) -> UserDto:
+    return self.user_repository.index_one(user_id)
+
