f7bad3ca9e5a344f16b6d12823bd30120784bec4
Daniel Braz <danielbrazlopes16@gmail.com>
2024-08-01 01:23:47 -0300

    feat(main): create use case for index users

---
+++ src/application/use_cases/index_users_use_case.py
@@ -0,0 +1,12 @@
+from injector import inject
+from src.application.interfaces.use_cases.index_users_use_case_interface import IndexUsersUseCaseInterface
+from src.application.interfaces.repositories.user_repository_interface import UserRepositoryInterface
+from src.application.dtos.user_dto import UserDto
+
+class IndexUsersUseCase(IndexUsersUseCaseInterface):
+  @inject
+  def __init__(self, user_repository: UserRepositoryInterface):
+    self.user_repository = user_repository
+
+  def execute(self) -> list[UserDto]:
+    return self.user_repository.index()
