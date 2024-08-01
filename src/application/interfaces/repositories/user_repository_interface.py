34c535fe155e5727bfb1f7e6652899f54ff6d352
Daniel Braz <danielbrazlopes16@gmail.com>
2024-07-31 21:15:05 -0300

    fix(main): typing user repository interface

--- src/application/interfaces/repositories/user_repository_interface.py
+++ src/application/interfaces/repositories/user_repository_interface.py
@@ -1,26 +1,27 @@
 from abc import ABC, abstractmethod
+from src.application.dtos.user_dto import UserDto
 
 class UserRepositoryInterface(ABC):
   @abstractmethod
-  def index(self):
+  def index(self) -> list[UserDto]:
     pass
 
   @abstractmethod
-  def index_one(self, user_id):
+  def index_one(self, user_id: str) -> UserDto:
     pass
 
   @abstractmethod
-  def index_by_email(self, user_email):
+  def index_by_email(self, user_email: str) -> UserDto:
     pass
 
   @abstractmethod
-  def store(self, user):
+  def store(self, user: UserDto) -> None:
     pass
 
   @abstractmethod
-  def update(self, user):
+  def update(self, user: UserDto) -> None:
     pass
 
   @abstractmethod
-  def delete(self, user_id):
+  def delete(self, user_id: str) -> None:
     pass
