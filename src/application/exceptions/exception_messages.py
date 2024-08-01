a583ee02cfb5be4032d2743134dd67f60edd6d43
Daniel Braz <danielbrazlopes16@gmail.com>
2024-08-01 01:08:25 -0300

    feat(main): create exception messages

---
+++ src/application/exceptions/exception_messages.py
@@ -0,0 +1,5 @@
+from src.application.interfaces.exceptions.exception_messages_interface import ExceptionMessagesInterface
+
+class ExceptionMessages(ExceptionMessagesInterface):
+  INTERNAL_SERVER_ERROR = 'Internal server error. Try again!'
+  USER_NOT_FOUND = 'User not founded'
