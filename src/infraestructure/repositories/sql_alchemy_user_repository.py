853c08109eeafafeb399d0c79c89ca43ba3a93cf
Daniel Braz <danielbrazlopes16@gmail.com>
2024-07-31 21:46:17 -0300

    fix(main): use user dto instead pass row structure to app layer

--- src/infraestructure/repositories/sql_alchemy_user_repository.py
+++ src/infraestructure/repositories/sql_alchemy_user_repository.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from src.infraestructure.datasources import engine
 from src.infraestructure.schemas.sql_alchemy_user_schema import SqlAlchemyUserSchema
 from src.application.interfaces.repositories.user_repository_interface import UserRepositoryInterface
+from src.application.dtos.user_dto import UserDto
 import uuid
 
 class SqlAlchemyUserRepository(UserRepositoryInterface):
@@ -9,20 +10,23 @@ class SqlAlchemyUserRepository(UserRepositoryInterface):
     Session = sessionmaker(bind=engine)
     self.session = Session()
 
-  def index():
+  def index() -> list[UserDto]:
     return
 
-  def index_one(self, user_id):
-    return self.session.query(SqlAlchemyUserSchema).filter_by(id=uuid.UUID(user_id)).first()
+  def index_by_id(self, user_id: str) -> UserDto | None:
+    user_entity = self.session.query(SqlAlchemyUserSchema).filter_by(id=uuid.UUID(user_id)).first()
 
-  def index_by_email(self, user_email):
+    if user_entity:
+      return UserDto(id=user_entity.id, name=user_entity.name, email=user_entity.email)
+
+  def index_by_email(self, user_email: str) -> UserDto | None:
     return
 
-  def store(self, user):
+  def store(self, user: UserDto) -> None:
     return
 
-  def update(self, user):
+  def update(self, user: UserDto) -> None:
     return
 
-  def delete(self, user_id):
+  def delete(self, user_id: str) -> None:
     return
