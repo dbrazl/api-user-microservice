94b9ca40f5db949b198565dedf295a4dbcc29b47
Daniel Braz <danielbrazlopes16@gmail.com>
2024-08-02 15:08:46 -0300

    feat(main): inject index users by name use case

--- src/infraestructure/di/container.py
+++ src/infraestructure/di/container.py
@@ -15,6 +15,8 @@ from src.application.interfaces.use_cases.index_users_use_case_interface import 
 from src.application.use_cases.index_users_use_case import IndexUsersUseCase
 from src.application.interfaces.use_cases.index_user_by_email_use_case_interface import IndexUserByEmailUseCaseInterface
 from src.application.use_cases.index_user_by_email_use_case import IndexUserByEmailUseCase
+from src.application.interfaces.use_cases.index_users_by_name_use_case_interface import IndexUsersByNameUseCaseInterface
+from src.application.use_cases.index_users_by_name_use_case import IndexUsersByNameUseCase
 
 class DI:
   @staticmethod
@@ -27,3 +29,4 @@ class DI:
     binder.bind(IndexUserByIdUseCaseInterface, to=IndexUserByIdUseCase, scope=singleton)
     binder.bind(IndexUsersUseCaseInterface, to=IndexUsersUseCase, scope=singleton)
     binder.bind(IndexUserByEmailUseCaseInterface, to=IndexUserByEmailUseCase, scope=singleton)
+    binder.bind(IndexUsersByNameUseCaseInterface, to=IndexUsersByNameUseCase, scope=singleton)
