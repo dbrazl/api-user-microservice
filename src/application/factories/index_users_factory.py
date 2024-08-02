92490c2050c8d92b9c0cb43ed7fb464e4967dd66
Daniel Braz <danielbrazlopes16@gmail.com>
2024-08-02 15:09:18 -0300

    feat(main): update IndexUsersFactory

--- src/application/factories/index_users_factory.py
+++ src/application/factories/index_users_factory.py
@@ -2,6 +2,7 @@ from injector import inject
 from src.application.interfaces.factories.index_users_factory_interface import IndexUsersFactoryInterface
 from src.application.interfaces.use_cases.index_user_by_id_use_case_interface import IndexUserByIdUseCaseInterface
 from src.application.interfaces.use_cases.index_user_by_email_use_case_interface import IndexUserByEmailUseCaseInterface
+from src.application.interfaces.use_cases.index_users_by_name_use_case_interface import IndexUsersByNameUseCaseInterface
 from src.application.interfaces.use_cases.index_users_use_case_interface import IndexUsersUseCaseInterface
 
 class IndexUsersFactory(IndexUsersFactoryInterface):
@@ -10,10 +11,12 @@ class IndexUsersFactory(IndexUsersFactoryInterface):
     self,
     index_user_by_id_use_case: IndexUserByIdUseCaseInterface,
     index_user_by_email_use_case: IndexUserByEmailUseCaseInterface,
-    index_users_use_case: IndexUsersUseCaseInterface,
+    index_users_by_name_use_case: IndexUsersByNameUseCaseInterface,
+    index_users_use_case: IndexUsersUseCaseInterface
   ) -> None:
     self.index_user_by_id_use_case = index_user_by_id_use_case
     self.index_user_by_email_use_case = index_user_by_email_use_case
+    self.index_users_by_name_use_case = index_users_by_name_use_case
     self.index_users_use_case = index_users_use_case
 
   def make_index_use_case(self, filter: str) -> IndexUserByIdUseCaseInterface | IndexUserByEmailUseCaseInterface | IndexUsersUseCaseInterface:
@@ -23,6 +26,9 @@ class IndexUsersFactory(IndexUsersFactoryInterface):
 
       case 'email':
         return self.index_user_by_email_use_case
+
+      case 'name':
+        return self.index_users_by_name_use_case
 
       case _:
         return self.index_users_use_case
