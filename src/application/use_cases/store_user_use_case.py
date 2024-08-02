8c6d2343bd2607987ec240f375c6d95b85aad8a5
Daniel Braz <danielbrazlopes16@gmail.com>
2024-08-02 17:02:10 -0300

    feat(main): create use case to store user

---
+++ src/application/use_cases/store_user_use_case.py
@@ -0,0 +1,12 @@
+from injector import inject
+from application.dtos.user_dto import UserDto
+from src.application.interfaces.use_cases.store_user_use_case_interface import StoreUserUseCaseInterface
+from src.application.interfaces.repositories.user_repository_interface import UserRepositoryInterface
+
+class StoreUserUseCase(StoreUserUseCaseInterface):
+  @inject
+  def __init__(self, user_repository: UserRepositoryInterface) -> None:
+    self.user_repository = user_repository
+
+  def execute(self, user: UserDto) -> None:
+    self.user_repository.store(user)
